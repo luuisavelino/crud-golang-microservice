2826dce5d5b717e3b855c08422945e5ded29eb9b
CloudBuildCICD <luuis.avelino@gmail.com>
2023-03-24 22:29:38 -0300

    Ajuste na criação e atualização de produtos

--- gestao-produtos/pkg/controllers/controllers.go
+++ gestao-produtos/pkg/controllers/controllers.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/luuisavelino/crud-golang-microservice-gestao-produtos/pkg/models"
@@ -26,23 +28,67 @@ func DeleteProduct(c *gin.Context) {
 }
 
 func CreateProduct(c *gin.Context) {
+	var product models.Product
 
 	c.Request.ParseMultipartForm(1000)
 	for key, value := range c.Request.PostForm {
-		fmt.Println(key, value)
+		switch key {
+		case "name":
+			product.Name = value[0]	
+		case "description":
+			product.Description = value[0]
+		case "price":
+			price, err := strconv.ParseFloat(value[0], 64)
+			if err != nil {
+				log.Println(err)
+			}
+			product.Price = price
+		case "amount":
+			amount, err := strconv.Atoi(value[0])
+			if err != nil {
+				log.Println(err)
+			}
+			product.Amount = amount
+		}
 	}
+
+	models.CriarNovoProduto(product)
 
 	c.JSON(http.StatusOK, "Item criado com sucesso!")
 }
 
 func UpdateProduct(c *gin.Context) {
 	var product models.Product
-	id := c.Params.ByName("id")
-
-	if err := c.BindJSON(&product); err != nil {
-		panic(err)
+	id, err := strconv.Atoi(c.Params.ByName("id"))	
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "Id do produto inválido!")
 	}
 
+	c.Request.ParseMultipartForm(1000)
+	for key, value := range c.Request.PostForm {
+		switch key {
+		case "name":
+			product.Name = value[0]	
+		case "description":
+			product.Description = value[0]
+		case "price":
+			price, err := strconv.ParseFloat(value[0], 64)
+			if err != nil {
+				log.Println(err)
+			}
+			product.Price = price
+		case "amount":
+			amount, err := strconv.Atoi(value[0])
+			if err != nil {
+				log.Println(err)
+			}
+			product.Amount = amount
+		}
+	}
+
+	product.Id = id
+
 	models.AtualizaProduto(product)
-	c.JSON(http.StatusOK, "Id "+id+" editado com sucesso!")
+
+	c.JSON(http.StatusOK, "Id "+string(id)+" editado com sucesso!")
 }
